d706d67b3d893c14eafd75b89195c3b405c94950
John <jawnnypoo@gmail.com>
2018-05-30 06:26:12 -0500

    Do the command

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
 import { Factorial } from './utils/factorial';
-import * as Path from 'path';
+import * as fs from 'fs'
 import axios from 'axios';
+
+const { spawnSync } = require('child_process')
 
 axios({
     method: 'get',
@@ -10,7 +12,15 @@ axios({
     responseType: 'text'
 })
     .then(function (response) {
-        console.log(response.data)
+        const version = response.data
+        console.log(`Upgrading to ${version}`)
+        if (fs.existsSync('gradlew') || fs.existsSync('gradlew.bat')) {
+            const gradle = spawnSync('gradlew.bat', ['-lh', '/usr']);
+            console.log(`stderr: ${gradle.stderr.toString()}`);
+            console.log(`stdout: ${gradle.stdout.toString()}`);
+        } else {
+            console.log('Gradle wrapper not found in current directory')
+        }
     })
     .catch(err => {
         console.log(err)
